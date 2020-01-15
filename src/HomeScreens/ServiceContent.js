2297284c0eb91f87ca51225c57c2be74a93e815e
Đặng Lê Minh Trường <dlmtruong1609@gmail.com>
2020-01-15 20:26:54 +0700

    fix aos

--- src/HomeScreens/ServiceContent.js
+++ src/HomeScreens/ServiceContent.js
@@ -1,8 +1,9 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import bia from '../resource/images/bia.jpg'
 const ServiceContent = () => {
+    // let bia = require("../resource/images/bia.jpg");
     return (
-      <div className="site-blocks-cover" style={{ backgroundimage:`url(require("images bia.jpg"))` }}data-aos="fade">
+      <div className="site-blocks-cover" style={{ backgroundImage: `url(${bia})` }}>
       <div className="container">
         <div className="row align-items-start align-items-md-center justify-content-end">
           <div className="col-md-5 text-center text-md-left pt-5 pt-md-0">
