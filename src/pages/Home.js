942f53b759ad6b6f317259e63269570f46900599
Đặng Lê Minh Trường <dlmtruong1609@gmail.com>
2020-01-15 21:24:36 +0700

    fix home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import Footer from '../component/Footer';
-import Header from '../component/Header';
 import ServiceContent from '../component/home/ServiceContent';
 
 const Home = () => {
     return (
         <div className="site-wrap">
-            <Header/>
             <ServiceContent/>
-            <Footer/>
         </div>
     );
 };
