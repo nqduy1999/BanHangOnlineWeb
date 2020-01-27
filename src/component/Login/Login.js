e99fd4d4fb1e9d1b32f12aabc83c4e81996fcad6
Duy <duy@Duys-MacBook-Pro.local>
2020-01-27 15:25:35 +0700

    update login

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -2,30 +2,23 @@ import React, {useState}from 'react';
 import { Link } from 'react-router-dom';
 import "../../resource/css/login.css"
 import { useForm } from "react-hook-form";
-const defaultlogin={
-    email:"",
-    pass:""
-}
 const Login = () => {
-    const { handleSubmit, login} = useForm({defaultlogin});
-    const [data, setData] = useState(null);
-
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => {
+      console.log(data);
+    }; // your form submit function which will invoke after successful validation
+  
     return (
-        <div>
-            <form onSubmit={handleSubmit(data => setData(data))} >
+            <form onSubmit={handleSubmit(onSubmit)} >
                 <label>Email</label>
-                <input name="email" type="email" ref={login}/>
+                <input name="email" type="email" ref={register}/>
+                {errors.pass && <p>Email không được để trống</p>}
                 <label>Mật Khẩu</label>
-                <input name="pass" type="password"/>
+                <input name="pass" type="password" ref={register({ required: true, maxLength: 10 })}/>
+                {errors.pass && <p>Mật khẩu không được để trống</p>}
                 <input type="submit" value="Đăng Nhập"/>
                 <Link to="/dangky"><p>Bạn chưa có tài khoản ? Nhấn vào đây để đăng ký </p></Link>
-                {console.log(data && (
-              <pre >
-                {JSON.stringify(data, null, 2)}
-              </pre>
-            ))}
             </form>
-        </div>
     );
 };
 
