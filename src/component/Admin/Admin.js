1174b8a0dfda84cbfcef0f5dae819361328f0478
Duy <duy@Duys-MacBook-Pro.local>
2020-01-28 23:15:50 +0700

    fix

--- src/component/Admin/Admin.js
+++ src/component/Admin/Admin.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import "../../resource/css/sb-admin-2.min.css"
+import '../../resource/css/fontawesome-free/css/all.min.css'
 import NavAdmin from './ToolboxAdmin';
+import AdminSearch from './AdminSearch';
 const Admin = () => {
     return (
-        <div id="wrapper">
-            <NavAdmin/>
-        </div>
+<div id="wrapper">
+       <NavAdmin/>
+    <div id="content-wrapper" className="d-flex flex-column">
+      {/* Main Content */}
+      <div id="content">
+<nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+          <AdminSearch/>
+</nav>      </div>
+    </div>
+</div>
+
     );
 };
 
