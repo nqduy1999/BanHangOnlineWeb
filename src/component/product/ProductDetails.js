482ad518157d91f1f3aed0687f0d18ba2655ad6d
Đặng Lê Minh Trường <dlmtruong1609@gmail.com>
2020-02-21 20:08:07 +0700

    handle method basic on ui

--- src/component/product/ProductDetails.js
+++ src/component/product/ProductDetails.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import Axios from 'axios';
-
+import  { useAlertService } from '../../services/useAlertService';
 import bao_thu_a3 from '../../resource/images/bao_thu_a3.jpg';
 const ProductDetails = () => {
   const [data, setData] = useState({
@@ -16,13 +16,21 @@ const ProductDetails = () => {
     hinhAnh: '',
     loaiSanPham: ''
   });
+  const [chiTietHoaDon, setChiTietHoaDon] = useState({
+    sanPham: data,
+    donGia: 0,
+    soLuong: 1
+  })
+  const [quantity, setQuantity] = useState(1);
+  const [checkQuantityErr, setCheckQuantityErr] = useState(false);
+  useAlertService("Thông báo", "Vui lòng nhập đúng số lượng phù hợp", "warning", checkQuantityErr);
   let useQuery = () => {
     return new URLSearchParams(useLocation().search);
   }
+  const [loading, setLoading] = useState(false);
   const id = useQuery();
   const url = `http://localhost:8080/api/quanly/sanpham/chitiet?id=${id.get("id")}`;
     let getProductDetail = async (source) => {
-      console.log(url);
       await Axios.get(url)
       .then(async res => {
         const products = await res.data;
@@ -36,6 +44,11 @@ const ProductDetails = () => {
           hinhAnh: products.hinhAnh,
           loaiSanPham: products.loaiSanPham
         });
+      await setChiTietHoaDon({...chiTietHoaDon,
+          sanPham: products,
+          donGia: products.giaSanPham * 1,
+          soLuong: 1
+        })
       }).catch(err => {
         if(Axios.isCancel(err)) {
           console.log(`Canceled`, err);
@@ -44,6 +57,53 @@ const ProductDetails = () => {
         }
       })
     }
+    let handleUpdateQuantity = (qt, action) => {
+      if(action === "tang") {
+        if(quantity >= 1 && quantity < data.soLuongTon) {
+          setChiTietHoaDon({...chiTietHoaDon,
+            sanPham: data,
+            donGia: data.giaSanPham * qt,
+            soLuong: qt
+          })
+          setQuantity(qt);
+        }
+      } else if(action === "giam") {
+        if(quantity > 1 && quantity <= data.soLuongTon) {
+          setChiTietHoaDon({...chiTietHoaDon,
+            sanPham: data,
+            donGia: data.giaSanPham * qt,
+            soLuong: qt
+          })
+          setQuantity(qt);
+        }
+      } 
+      if(quantity > data.soLuongTon) {
+        setChiTietHoaDon({...chiTietHoaDon,
+          sanPham: data,
+          donGia: data.giaSanPham * data.soLuongTon,
+          soLuong: data.soLuongTon
+        })
+        setQuantity(data.soLuongTon);
+      } else if(quantity <= 0) {
+        setChiTietHoaDon({...chiTietHoaDon,
+          sanPham: data,
+          donGia: data.giaSanPham * 1,
+          soLuong: 1
+        })
+        setQuantity(1);
+      }
+    }
+    let onAddOrderDetailsToShoppingCard = () => {
+      if(quantity > 0 && quantity <= data.soLuongTon) {
+        setChiTietHoaDon({...chiTietHoaDon,
+          sanPham: data,
+          donGia: data.giaSanPham * quantity,
+          soLuong: quantity
+        })
+      } else {
+        setCheckQuantityErr(true);
+      }
+    }
     useEffect(() => {
       const source = Axios.CancelToken.source(); // huỷ request (Rất quan trọng)
       getProductDetail(source);
@@ -51,7 +111,7 @@ const ProductDetails = () => {
         source.cancel();
       };
     }, [url])
-    return (
+    return data.giaSanPham!=0 ? (
         <div>
           <div className="site-section">
             <div className="container">
@@ -65,24 +125,24 @@ const ProductDetails = () => {
                   <p className="mb-4">Ex numquam veritatis debitis minima quo error quam eos dolorum quidem perferendis. Quos repellat dignissimos minus, eveniet nam voluptatibus molestias omnis reiciendis perspiciatis illum hic magni iste, velit aperiam quis.</p>
                   <p><strong className="text-primary h4">{data.giaSanPham}</strong></p>
                   <div className="mb-5 ml-5">
-                    <div className="input-group mb-3" style={{maxWidth: 120}}>
+                    <div className="input-group mb-3" style={{maxWidth: 200}}>
                       <div className="input-group-prepend">
-                        <button className=" btn-outline-primary js-btn-minus" type="button">−</button>
+                        <button className=" btn-outline-primary js-btn-minus" type="button" onClick={() => {handleUpdateQuantity(quantity-1, "giam");}}>−</button>
                       </div>
-                      <input type="text" className="form-control text-center" defaultValue={1} aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                      <input type="text" className="form-control text-center" onChange={e => {handleUpdateQuantity(Number(e.target.value), "tang");}} value={quantity} aria-label="Example text with button addon" aria-describedby="button-addon1" />
                       <div className="input-group-append">
-                        <button className=" btn-outline-primary js-btn-plus" type="button">+</button>
+                        <button className=" btn-outline-primary js-btn-plus" type="button" onClick={() => {handleUpdateQuantity(quantity+1, "tang");}}>+</button>
                       </div>
                     </div>
                   </div>
-                  <p><Link to="/giohang" className="buy-now  btn btn-sm btn-primary">Thêm vào giỏ</Link></p>
+                  <p><Link to="/giohang"  className="buy-now  btn btn-sm btn-primary" onClick={onAddOrderDetailsToShoppingCard}>Thêm vào giỏ</Link></p>
                 </div>
               </div>
             </div>
           </div>
 
         </div>
-    );
+    ) : "Loading";
 };
 
 export default ProductDetails;
